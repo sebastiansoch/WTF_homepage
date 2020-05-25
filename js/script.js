3a78fcf41f9f5b9bde47a1975550e4e14ee110f1
Sebastian Soch <sebastian.soch@gmail.com>
2020-05-25 18:04:24 +0200

    WTF 03.04
    Added simple script, just to say hello in console tag
    Fixed css

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const name = 'Sebastian Soch';
+const age = 41;
+
+console.log(`Hej żeglarzu, witaj ma mojej stronie. Nazywam się ${name} i mam ${age} lat!`);
+console.log('Czyż to nie była super istotna informacja dla Twojego dalszego rozoju');
