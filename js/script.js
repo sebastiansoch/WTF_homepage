6e8feb53f31515c66d6489793155b02de1d66044
Sebastian Soch <sebastian.soch@gmail.com>
2020-05-25 22:16:27 +0200

    WTF 03.05
    Header, footer and hx received classes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-const name = 'Sebastian Soch';
+const name = "Sebastian Soch";
 const age = 41;
 
-console.log(`Hej żeglarzu, witaj ma mojej stronie. Nazywam się ${name} i mam ${age} lat!`);
-console.log('Czyż to nie była super istotna informacja dla Twojego dalszego rozoju');
+console.log(
+  `Hej żeglarzu, witaj ma mojej stronie. Nazywam się ${name} i mam ${age} lat!`
+);
+console.log(
+  "Czyż to nie była super istotna informacja dla Twojego dalszego rozoju"
+);
