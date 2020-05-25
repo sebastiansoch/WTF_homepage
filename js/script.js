8dae7d31a993d6ae785689777da6c91bc653fad5
Sebastian Soch <sebastian.soch@gmail.com>
2020-05-25 22:31:43 +0200

    WTF 03.06
    VSCode settings - single quote set

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-const name = "Sebastian Soch";
+const name = 'Sebastian Soch';
 const age = 41;
 
 console.log(
   `Hej żeglarzu, witaj ma mojej stronie. Nazywam się ${name} i mam ${age} lat!`
 );
 console.log(
-  "Czyż to nie była super istotna informacja dla Twojego dalszego rozoju"
+  'Czyż to nie była super istotna informacja dla Twojego dalszego rozoju'
 );
